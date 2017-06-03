9e24447e743d75dcf7ebff365f7c961d434c2ca6
ktserpes <1q2w3e4r>
2017-06-03 11:03:13 +0300

    Initial commit

---
+++ app/src/main/java/gr/uoa/eclass/android/myfirstapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package gr.uoa.eclass.android.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String userText = editText.getText().toString();
+                TextView textView = (TextView)findViewById(R.id.textView);
+                textView.setText(userText);
+            }
+        });
+    }
+}
